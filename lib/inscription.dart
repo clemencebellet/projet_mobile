6b652c2a786ae1e98bb49005377d7ee22f394a12
clemencebellet <78761529+clemencebellet@users.noreply.github.com>
2023-02-22 17:24:25 +0100

    Ajout possibilité de s'inscrire

--- lib/inscription.dart
+++ lib/inscription.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Inscription extends StatefulWidget {
   const Inscription({super.key});
@@ -13,74 +13,69 @@ class Inscription extends StatefulWidget {
 class _InscriptionState extends State<Inscription> {
   @override
   Widget build(BuildContext context) {
+    TextEditingController emailController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
+    TextEditingController usernameController = TextEditingController();
+    TextEditingController verifpasswordController = TextEditingController();
+
     return Scaffold(
         body: AnnotatedRegion<SystemUiOverlayStyle>(
-          value: SystemUiOverlayStyle.light,
-
-          child: Stack(
-            children: <Widget>[
-              Container(
-                  height: double.infinity,
-                  width: double.infinity,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("images/background.png"),
-                        fit: BoxFit.cover),
-                  ),
-                  child: SingleChildScrollView(
-
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 25,
-                        vertical: 92.73
+      value: SystemUiOverlayStyle.light,
+      child: Stack(
+        children: <Widget>[
+          Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("images/background.png"),
+                    fit: BoxFit.cover),
+              ),
+              child: SingleChildScrollView(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 25, vertical: 92.73),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text(
+                      textAlign: TextAlign.center,
+                      'Inscription',
+                      style: TextStyle(
+                        fontFamily: "GoogleSans-Bold",
+                        color: Colors.white,
+                        fontSize: 30.53169,
+                      ),
                     ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                    const SizedBox(height: 10),
+                    const Text(
+                      textAlign: TextAlign.center,
+                      'Veuillez saisir ces différentes informations, '
+                      'afin que vos listes soient sauvegardées.',
+                      style: TextStyle(
+                        fontFamily: "ProximaNova-Regular",
+                        color: Colors.white,
+                        fontSize: 15.265845,
+                      ),
+                    ),
+                    const SizedBox(height: 40),
+
+                    // -----------------BOX POUR LE NOM -----------------------
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        const Text(
-                          textAlign: TextAlign.center,
-                          'Inscription',
-                          style: TextStyle(
-                            fontFamily: "GoogleSans-Bold",
-                            color: Colors.white,
-                            fontSize: 30.53169,
-
-                          ),
-                        ),
                         const SizedBox(height: 10),
-                        const Text(
-                          textAlign: TextAlign.center,
-                          'Veuillez saisir ces différentes informations, '
-                          'afin que vos listes soient sauvegardées.',
-                          style: TextStyle(
-                            fontFamily: "ProximaNova-Regular",
-                            color: Colors.white,
-                            fontSize: 15.265845,
-                          ),
-                        ),
-                        const SizedBox(height: 40),
-
-                       // -----------------BOX POUR LE NOM -----------------------
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-
-                        children: <Widget>[
-
-                          const SizedBox(height: 10),
-                          Container(
+                        Container(
                             alignment: Alignment.centerLeft,
                             decoration: BoxDecoration(
                               color: Color(0xFF1E262C),
                               borderRadius: BorderRadius.circular(3.52),
-
                             ),
                             height: 46.89,
-
-                            child: const TextField(textAlign: TextAlign.center,
-                                keyboardType: TextInputType.emailAddress,
-                                style: TextStyle(
-                                    color: Colors.white
-                                ),
-                                decoration: InputDecoration(
+                            child: TextField(
+                                textAlign: TextAlign.center,
+                                controller: usernameController,
+                                style: const TextStyle(color: Colors.white),
+                                decoration: const InputDecoration(
                                     border: InputBorder.none,
                                     //contentPadding: EdgeInsets.only(top:10),
 
@@ -89,179 +84,163 @@ class _InscriptionState extends State<Inscription> {
                                       color: Colors.white,
                                       fontFamily: 'ProximaNova-Regular',
                                       fontSize: 15.239016,
-                                    )
-
-                                )
-                            ),
-                          )
-                        ],
-                      ),
-
+                                    ))))
+                      ],
+                    ),
 
 // -----------------------BOX POUR EMAIL ---------------------------------------------
-                      Column(
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        const SizedBox(height: 10),
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          decoration: BoxDecoration(
+                            color: Color(0xFF1E262C),
+                            borderRadius: BorderRadius.circular(3.52),
+                          ),
+                          height: 46.89,
+                          child: TextField(
+                              textAlign: TextAlign.center,
+                              controller: emailController,
+                              style: const TextStyle(color: Colors.white),
+                              decoration: const InputDecoration(
+                                  border: InputBorder.none,
+                                  //contentPadding: EdgeInsets.only(top:10),
+
+                                  hintText: 'E-mail',
+                                  hintStyle: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'ProximaNova-Regular',
+                                    fontSize: 15.239016,
+                                  ))),
+                        )
+                      ],
+                    ),
+
+                    // -----------------------BOX POUR MDP ---------------------------------------------
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        const SizedBox(height: 10),
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          decoration: BoxDecoration(
+                            color: Color(0xFF1E262C),
+                            borderRadius: BorderRadius.circular(3.52),
+                          ),
+                          height: 46.89,
+                          child: TextField(
+                              textAlign: TextAlign.center,
+                              controller: passwordController,
+                              obscureText: true,
+                              style: const TextStyle(color: Colors.white),
+                              decoration: const InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: 'Mot de passe',
+                                  hintStyle: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'ProximaNova-Regular',
+                                    fontSize: 15.239016,
+                                  ))),
+                        )
+                      ],
+                    ),
+// -----------------------BOX POUR VERFI MDP ---------------------------------------------
+                    Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
-
                         children: <Widget>[
-
                           const SizedBox(height: 10),
                           Container(
                             alignment: Alignment.centerLeft,
                             decoration: BoxDecoration(
                               color: Color(0xFF1E262C),
                               borderRadius: BorderRadius.circular(3.52),
-
                             ),
                             height: 46.89,
-
-                            child: const TextField(textAlign: TextAlign.center,
-
-                                style: TextStyle(
-                                    color: Colors.white
-                                ),
-                                decoration: InputDecoration(
+                            child: TextField(
+                                textAlign: TextAlign.center,
+                                obscureText: true,
+                                controller: verifpasswordController,
+                                style: const TextStyle(color: Colors.white),
+                                decoration: const InputDecoration(
                                     border: InputBorder.none,
-                                    //contentPadding: EdgeInsets.only(top:10),
-
-                                    hintText: 'E-mail',
+                                    hintText: 'Vérification du mot de passe',
                                     hintStyle: TextStyle(
                                       color: Colors.white,
                                       fontFamily: 'ProximaNova-Regular',
                                       fontSize: 15.239016,
-                                    )
-
-                                )
-                            ),
-                          )
-                        ],
-                      ),
-
-                        // -----------------------BOX POUR MDP ---------------------------------------------
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-
-                      children: <Widget>[
-
-                        const SizedBox(height: 10),
-                        Container(
-                          alignment: Alignment.centerLeft,
-                          decoration: BoxDecoration(
-                            color: Color(0xFF1E262C),
-                            borderRadius: BorderRadius.circular(3.52),
-
+                                    ))),
                           ),
-                          height: 46.89,
+                        ]),
 
-                          child: const TextField(textAlign: TextAlign.center,
-                              obscureText: true,
-                              style: TextStyle(
-                                  color: Colors.white
-                              ),
-                              decoration: InputDecoration(
-                                  border: InputBorder.none,
-
-
-                                  hintText: 'Mot de passe',
-                                  hintStyle: TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: 'ProximaNova-Regular',
-                                    fontSize: 15.239016,
-                                  )
-
-                              )
-                          ),
-                        )
-                      ],
-                    ),
-// -----------------------BOX POUR VERFI MDP ---------------------------------------------
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-
-                      children: <Widget>[
-
-                        const SizedBox(height: 10),
-                        Container(
-                          alignment: Alignment.centerLeft,
-                          decoration: BoxDecoration(
-                            color: Color(0xFF1E262C),
-                            borderRadius: BorderRadius.circular(3.52),
-
-                          ),
-                          height: 46.89,
-
-                          child: const TextField(textAlign: TextAlign.center,
-                              obscureText: true,
-                              style: TextStyle(
-                                  color: Colors.white
-                              ),
-                              decoration: InputDecoration(
-                                  border: InputBorder.none,
-
-
-                                  hintText: 'Vérification du mot de passe',
-                                  hintStyle: TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: 'ProximaNova-Regular',
-                                    fontSize: 15.239016,
-                                  )
-
-                              )
-                          ),
-                        )
-                      ],
-                    ),
-
-                        // -----------------------BOX POUR BOUTON INSCRIRE ---------------------------------------------
+                    // -----------------------BOX POUR BOUTON INSCRIRE ---------------------------------------------
 
                     Column(
                       children: <Widget>[
-
                         const SizedBox(height: 100),
                         Container(
-
                             alignment: Alignment.centerLeft,
                             decoration: BoxDecoration(
                               color: Color(0xFF636af6),
                               borderRadius: BorderRadius.circular(3.52),
-
                             ),
                             height: 46.89,
                             child: TextButton(
+                                style: TextButton.styleFrom(),
+                                onPressed: () {
+                                  if (passwordController.text !=
+                                      verifpasswordController.text) {
+                                    print(passwordController);
+                                    print(verifpasswordController);
+                                    showDialog(
+                                        context: context,
+                                        barrierDismissible: false,
+                                        builder: (BuildContext context) {
+                                          return AlertDialog(
+                                            title: const Text('Erreur'),
+                                            content: const Text(
+                                                "Les mots de passe ne correspondent pas "),
+                                            actions: [
+                                              TextButton(
+                                                  onPressed: () {
+                                                    Navigator.of(context).pop();
+                                                  },
+                                                  child: const Text('Fermer'))
+                                            ],
+                                          );
+                                        });
+                                  } else {
+                                    FirebaseAuth.instance
+                                        .createUserWithEmailAndPassword(
+                                            email: emailController.text,
+                                            password: passwordController.text)
+                                        .then((value) => ((value) {
+                                              print("Nouveau Compte crée ");
 
-                                style: TextButton.styleFrom(
-                                ),
-                                onPressed: () {},
-
-                                child:  const Center(
+                                            }));
+                                    Navigator.pushNamed(
+                                        context, '/accueil');
+                                  }
+                                },
+                                child: const Center(
                                   child: Text(
-                                    textAlign:TextAlign.center,
-
+                                    textAlign: TextAlign.center,
                                     style: TextStyle(
                                       color: Colors.white,
-
                                       fontFamily: 'ProximaNova-Regular',
                                       fontSize: 15.239016,
                                     ),
                                     "S'inscrire",
-
-
                                   ),
-
-                                )
-                            )
-
-                        )
+                                )))
                       ],
                     ),
-
-                      ],
-                    ),
-                  )
-              )
-            ],
-          ),
-
-        )
-    );
+                  ],
+                ),
+              ))
+        ],
+      ),
+    ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class MyApp extends StatelessWidget{
                 builder : (context, snapshot){
                   if(snapshot.connectionState == ConnectionState.done)
                   {
-                    return Accueil();
+                      return Connexion();
                   }
                   return const Center(child : CircularProgressIndicator(),
                   );
